496c8cb1a93deec745a392bacbf31b47987e922e
CyanousSomdev <47048279+CyanousSomdev@users.noreply.github.com>
2019-01-26 09:14:51 +0530

    print statements

--- GITProjectTest1/src/com/nt/servlet/GitTest.java
+++ GITProjectTest1/src/com/nt/servlet/GitTest.java
@@ -27,6 +27,7 @@ public class GitTest extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("changed");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
